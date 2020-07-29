746cefee0221b06fdd3c75fd661bd66e1cc004bc
Marcos Robles <ecko_lob@outlook.com>
2020-07-28 21:22:24 -0500

    Ading Roles to a user

--- IdentityProject/Controllers/UserController.cs
+++ IdentityProject/Controllers/UserController.cs
@@ -1,7 +1,11 @@
-﻿using Seervice;
+﻿using Microsoft.AspNet.Identity.Owin;
+using Model;
+using Seervice;
+using Seervice.Auth;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,10 +15,79 @@ namespace IdentityProject.Controllers
     {
 
         private readonly UserService _userService = new UserService();
+        
+
+        private ApplicationRoleManager _roleManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Get<ApplicationRoleManager>();
+            }
+        }
+
+        private ApplicationUserManager _userManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Get<ApplicationUserManager>();
+            }
+        }
 
         public ActionResult Index()
         {
-            return View(_userService.GetAll()) ;
+            return View(_userService.GetAll());
         }
+
+        public ActionResult Get(string id)
+        {
+            var roles = _roleManager.Roles.Where(x => x.Enabled).ToList();
+
+            List<SelectListItem> dropDownItems = new List<SelectListItem>();
+            foreach (var role in roles)
+            {
+                SelectListItem item =    new SelectListItem { Value = role.Id, Text = role.Name };
+                dropDownItems.Add(item);
+            }
+            ViewBag.Roles = dropDownItems;
+
+            return View(_userService.Get(id));
+        }
+
+
+        public async Task<ActionResult> AddRoleTouser(string Id, string role) {
+
+            role = "User";
+            var roles = await _userManager.GetRolesAsync(Id);
+            if (roles.Any())
+            {
+                throw new Exception("El rol actual ya existe para el usuario");
+            }
+
+            await _userManager.AddToRoleAsync(Id,role);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task CreateRoles()
+        {
+
+            var roles = new List<ApplicationRole> {
+        new ApplicationRole{ Name = "Admin"},
+        new ApplicationRole{ Name = "Moderator"},
+        new ApplicationRole{ Name = "User"}
+        };
+
+            foreach (var role in roles)
+            {
+
+                if (!await _roleManager.RoleExistsAsync(role.Name))
+                {
+                    var x = await _roleManager.CreateAsync(role);
+                }
+            }
+
+        }
+
     }
+
 }
